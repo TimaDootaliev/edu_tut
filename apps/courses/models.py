b3a74c9168e18782587ac8c1f5595be2370ad3b3
timur <timur.dootaliev.td@gmail.com>
2022-04-07 10:55:48 +0600

    content

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
 
 from apps.courses.fields import OrderField
 
@@ -73,6 +74,10 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+
+    def render(self):
+        return render_to_string(f'courses/content/{self._meta.model_name}', {'item': self})
+
 
 
 class Text(ItemBase):
